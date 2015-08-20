9d737d0fd0e5de318569400e7c60b007a4f60a64
Henri Verroken <henriverroken@gmail.com>
2015-08-20 23:05:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Asset
+
+Append files to Go binaries
+
+Taken from 
+
+ - http://www.ueber.net/who/mjl/blog/p/assets-in-go-binaries
+ - https://bitbucket.org/mjl/asset/src
+
+and fixed includes.
---
+++ failfs.go
@@ -0,0 +1,32 @@
+package asset
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/tools/godoc/vfs"
+)
+
+type failFS struct {
+	err error
+}
+
+func failfs(err error) vfs.FileSystem {
+	return &failFS{fmt.Errorf("asset: %s", err.Error())}
+}
+
+func (fs *failFS) Open(name string) (vfs.ReadSeekCloser, error) {
+	return nil, fs.err
+}
+func (fs *failFS) Lstat(path string) (os.FileInfo, error) {
+	return nil, fs.err
+}
+func (fs *failFS) Stat(path string) (os.FileInfo, error) {
+	return nil, fs.err
+}
+func (fs *failFS) ReadDir(path string) ([]os.FileInfo, error) {
+	return nil, fs.err
+}
+func (fs *failFS) String() string {
+	return fmt.Sprintf("failfs(%q)", fs.err)
+}
